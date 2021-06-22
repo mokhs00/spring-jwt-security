666f27a47b8b6b047c0905e729fb80b466edb35b
mokhs00 <mokhs00@naver.com>
2021-06-22 15:27:08 +0900

    add: @Build to Authority

--- src/main/java/com/example/springjwtsecurity/domain/Authority.java
+++ src/main/java/com/example/springjwtsecurity/domain/Authority.java
@@ -1,6 +1,7 @@
 package com.example.springjwtsecurity.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @Entity
+@Builder
 public class Authority {
 
     @Id
