ff5662b78cab72418eaf8d66818005ea1b405b34
mokhs00 <mokhs00@naver.com>
2021-06-29 23:46:34 +0900

    add: 유저 이름으로 검색 단위 테스트

--- src/test/java/com/example/springjwtsecurity/service/CustomUserDetailsServiceTest.java
+++ src/test/java/com/example/springjwtsecurity/service/CustomUserDetailsServiceTest.java
@@ -3,11 +3,14 @@ package com.example.springjwtsecurity.service;
 import com.example.springjwtsecurity.domain.Authority;
 import com.example.springjwtsecurity.domain.User;
 import com.example.springjwtsecurity.repository.UserJpaRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,28 +23,34 @@ import java.util.Set;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomUserDetailsServiceTest {
 
-    @InjectMocks
-    private CustomUserDetailsService userDetailsService;
 
     @Mock
-    private UserJpaRepository userJpaRepository;
+    private CustomUserDetailsService userDetailsService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
 
-//    @Test
+    @DisplayName("유저 이름으로 검색")
+    @Test
     public void loadUserByUsername() {
         // given
         User user = new User(1L, "user", "pwd1", "mokhs", true,
                 Set.of(new Authority("ROLE_USER")));
 
         // when
-        when(this.userDetailsService.loadUserByUsername(anyString()))
-                .thenReturn(new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                        List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+
+        doReturn(new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))))
+                .when(userDetailsService).loadUserByUsername(anyString());
 
         // then
         UserDetails userDetails = this.userDetailsService.loadUserByUsername(user.getUsername());
